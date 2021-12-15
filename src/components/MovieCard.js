057d351773eba5dd18809fac2519ff290c4f8222
Randy Gulak <randy@randygulak.com>
2021-12-15 15:47:53 -0800

    added api key workshop movies is working

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,21 @@
-const MovieCard = () => {
-  return <div>Ay it's da emoji movie</div>;
+import { checkKey, getPopular, imageURL } from "../utilities/api";
+import src from "../images/emojiMovie.jpg";
+
+// Inside data
+// data.id
+// data.original_title
+// data.vote_average
+// data.vote_count
+// data.release_date
+// data.overview
+
+const MovieCard = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="image-cards-container">
+      <img src={`${imageURL}${data.poster_path}`} />
+    </div>
+  )
 };
 
 export default MovieCard;
