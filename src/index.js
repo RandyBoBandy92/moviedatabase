1610968e9e0e76fcb3679b5405241aae719a69bc
Randy Gulak <randy@randygulak.com>
2022-01-12 13:07:27 -0800

    moved workshop content into Home page component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Workshop from "./Workshop";
 import "./styles/styles.scss"
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="workshop" exact element={<Workshop/>}/>
-        <Route path="/" exact element={<App/>}/>
+        <Route path="/" exact element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/Home.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import MovieCard from "../components/MovieCard";
+import { checkKey, getPopular, getConfigData, getNowPlaying, getUpcoming } from "../utilities/api";
+
+
+
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState(false);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState(false);
+  const [upcomingMovies, setUpcomingMovies] = useState(false);
+
+
+  useEffect(() => {
+
+    getPopular()
+    .then((data) => setPopularMovies(data.results))
+    .catch(error => console.log(error))
+
+    getNowPlaying()
+    .then(data => setNowPlayingMovies(data.results) )
+    .catch(error => console.log(error))
+
+    getUpcoming()
+    .then(data => {
+      setUpcomingMovies(data.results)} )
+    .catch(error => console.log(error))
+    
+
+  }, []);
+
+//   console.log(getConfigData());
+
+
+  const settings = {
+    dots: false,
+    arrows: true,
+    swipeToSlide: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+  };
+  return (
+    <div className="wrapper">
+      <h2>Movie Database</h2>
+      <di></di>
+      <div className="movies-container" style={{padding: "0 25px"}}>
+        <h2>Popular</h2>
+        {popularMovies ? (
+          <Slider {...settings}>
+            {popularMovies.map((movieData) => (
+              <MovieCard data={movieData} />
+            ))}
+          </Slider>
+        ) : (
+          <h2>No Movies</h2>
+        )}
+      </div>
+      <div className="movies-container" style={{padding: "0 25px"}}>
+        <h2>Now Playing</h2>
+        {nowPlayingMovies ? (
+          <Slider {...settings}>
+            {nowPlayingMovies.map((movieData) => (
+              <MovieCard data={movieData} />
+            ))}
+          </Slider>
+        ) : (
+          <h2>No Movies</h2>
+        )}
+      </div>
+      <div className="movies-container" style={{padding: "0 25px"}}>
+        <h2>Upcoming</h2>
+        {upcomingMovies ? (
+          <Slider {...settings}>
+            {upcomingMovies.map((movieData) => (
+              <MovieCard data={movieData} />
+            ))}
+          </Slider>
+        ) : (
+          <h2>No Movies</h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
