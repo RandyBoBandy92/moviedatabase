999361d596efcdae77cc07685cf457e545ee4817
Randy Gulak <randy@randygulak.com>
2022-01-27 14:34:43 -0800

    basics of about

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import MoviePage from "../pages/MoviePage";
+import About from "../pages/About";
 import Workshop from "../Workshop";
 
 const AppRouter = () => {
@@ -9,6 +10,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="workshop" exact element={<Workshop />} />
         <Route path="/movie/:id" exact element={<MoviePage />} />
+
+        <Route path="/about" exact element={<About />} />
         <Route path="/" exact element={<Home />} />
       </Routes>
     </BrowserRouter>
