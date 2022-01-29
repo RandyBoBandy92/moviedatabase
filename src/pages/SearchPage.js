f9223b2640b82254d5b05518e739e26ba7d1ffdd
Randy Gulak <randy@randygulak.com>
2022-01-28 20:07:54 -0800

    adding recommended movies

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,39 +1,88 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { searchMovies } from "../utilities/api";
+import { getRecommendedMovies, searchMovies } from "../utilities/api";
 import MovieCard from "../components/MovieCard";
-import Header from "../components/Header"
+import Header from "../components/Header";
+import MoviesContainer from "../components/MoviesContainer";
 
 const SearchPage = () => {
   const { query } = useParams();
   const [movies, setMovies] = useState([]);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+
+  const formatQuery = (query) => {
+    // split the query up
+    const queryArray = query.split(" ");
+    for (let i = 0; i < queryArray.length; i++) {
+      queryArray[i] = queryArray[i].toLowerCase();
+      queryArray[i] = queryArray[i][0].toUpperCase() + queryArray[i].substr(1);
+    }
+
+    return queryArray.join(" ");
+  };
+
 
   useEffect(() => {
+    // grab movies from query
     searchMovies(query)
       .then((data) => {
-        console.log(data);
         setMovies(data.results);
+        if (data.results.length > 0) {
+          // we can search for recommended
+          const recommendationId = data.results[0].id;
+          getRecommendedMovies(recommendationId)
+            .then((data) => {
+              console.log(data);
+              setRecommendedMovies(data.results);
+            })
+            .catch((error) => console.log(error));
+        } else {
+          // no results, we should clear recommended movies
+          setRecommendedMovies([]);
+        }
       })
       .catch((error) => console.log(error));
+    // grab recommended movies
   }, [query]);
-  console.log(movies);
+  // console.log(movies);
   return (
-      <>
-    <div className="wrapper">
-        <Header/>
+    <>
+      <div className="wrapper">
+        <Header />
         <main>
-          <h1>Search results for:</h1>
-          <h2>{query}</h2>
-          <div className="search-movies">
-            {/* <h2>Search results for:{query}</h2> */}
-            {movies
-              .filter((moviedata) => moviedata.poster_path)
-              .map((moviedata) => (
-                <MovieCard data={moviedata} key={moviedata.id} />
-              ))}
-          </div>
+          {movies.length > 0 ? (
+            <>
+              <section className="search-results-section">
+                <div className="search-text-container">
+                  <h2 className="searchHeading">Search results for:</h2>
+                  <h1 className="searchQuery">{formatQuery(query)}</h1>
+                </div>
+                <div className="search-movies">
+                  {/* <h2>Search results for:{query}</h2> */}
+                  {movies
+                    .filter((moviedata) => moviedata.poster_path)
+                    .map((moviedata) => (
+                      <MovieCard data={moviedata} key={moviedata.id} />
+                    ))}
+                </div>
+              </section>
+              {recommendedMovies.length > 0 ? (
+                <section className="recommended-section">
+                  <h2>
+                    If you liked {movies[0].original_title} then you gonna love
+                    this:
+                  </h2>
+                  <MoviesContainer movies={recommendedMovies} />
+                </section>
+              ) : null}
+            </>
+          ) : (
+            <section>
+              <h2 className="search-result-heading">No Movies Found :(</h2>
+            </section>
+          )}
         </main>
-    </div>
+      </div>
     </>
   );
 };
--- src/utilities/api.js
+++ src/utilities/api.js
@@ -54,9 +54,16 @@ const getConfigData = async () => {
 };
 
 const searchMovies = async (query) => {
-  const searchUrl =     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
-  console.log(searchUrl)
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+  console.log(searchUrl);
   const response = await fetch(searchUrl);
+  return response.json();
+};
+
+const getRecommendedMovies = async (movieId) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`
+  );
   return response.json();
 };
 
@@ -69,6 +76,7 @@ export {
   getNowPlaying,
   searchMovies,
   getMovie,
+  getRecommendedMovies,
   imageURL,
   URL_SEARCH,
   originalImageURL,
