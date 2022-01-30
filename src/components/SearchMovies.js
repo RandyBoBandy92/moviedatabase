8e68432c4181f910f67e877276565872803ea9cb
Ryan Gulak <ryan.gulak@gmail.com>
2022-01-30 09:05:44 -0800

    search component now uses state, and has auto focus

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Search } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 
 function SearchMovies() {
   const [query, setQuery] = useState("");
+  const [navOpen, setNavOpen] = useState(false);
+  const ref = useRef();
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // searchMovies(query)
-    //   .then((data) => {
-    //     setMovies(data.results);
-    //   })
-    //   .catch((error) => console.log(error));
-    if (query) {
+    if (query && navOpen) {
       const uriQuery = encodeURIComponent(query);
       navigate(`/search/${uriQuery}`);
+    }
+    if (!navOpen) {
+      setNavOpen(true);
+      ref?.current?.focus();
     }
   };
 
@@ -26,9 +27,14 @@ function SearchMovies() {
     // that way, clicking or pressing enter on the keyboard will submit the form
     // this also works on mobile devices
 
-    <form onSubmit={handleSubmit} className="searchBox">
+    <form
+      onSubmit={handleSubmit}
+      className={`searchBox ${navOpen ? "active" : ""}`}
+      onBlur={() => setNavOpen(false)}
+    >
       <input
         className="searchInput"
+        ref={ref}
         type="text"
         name="query"
         placeholder="Movie..."
