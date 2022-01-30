8ea7e5f43cd3e8409d70b49b9bcddd016d112ecb
CiaranMcGuigan <ciaranmcguigan35@gmail.com>
2022-01-30 05:35:02 -0800

    updated mobile nav menu
    replaced icons and adjusted some settings in sass

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { IoMdHeart  } from 'react-icons/io';
+import { ImHome3  } from 'react-icons/im';
+import { FaQuestion } from 'react-icons/fa';
 
 const Nav = ({ navOpen, isDesktop, showHideNav }) => {
   return (
@@ -8,15 +11,15 @@ const Nav = ({ navOpen, isDesktop, showHideNav }) => {
       <div onClick={() => showHideNav()} className="nav-component">
         <div className="hamburger-menu"><span className="hamburger-line"></span></div>
       </div>
-      <ul>
+      <ul >
         <li className="nav-component home-link">
-          <Link to="/">🏠</Link>
+          <Link className="link" to="/"><ImHome3 className="nav-icon"/></Link>
         </li>
         <li className="nav-component favourites-link">
-          <Link to="/favourites">💖</Link>
+          <Link className="link" to="/favourites"><IoMdHeart className="nav-icon"/></Link>
         </li>
         <li className="nav-component about-link">
-          <Link to="/about">❓</Link>
+          <Link className="link" to="/about"><FaQuestion className="nav-icon"/></Link>
         </li>
       </ul>
     </nav>
