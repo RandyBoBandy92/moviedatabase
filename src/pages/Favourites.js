6fed549cb6dd88d5cc7585ebd562f15f06d10517
Randy Gulak <randy@randygulak.com>
2022-01-31 09:17:19 -0800

    added random related favourite component to favourites page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,10 +1,26 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Header from "../components/Header";
 import { GlobalContext } from "../GlobalState";
 import MoviesContainer from "../components/MoviesContainer";
+import { generateRandomIndex, sanitizeVideoData } from "../utilities/toolbelt";
+import { getRecommendedMovies } from "../utilities/api";
 
 const FavouritesPage = () => {
   const { favourites, addFavourite, delFavourite } = useContext(GlobalContext);
+  const [recommendedMovies, setRecommendedMovies] = useState(false);
+  useEffect(() => {
+    if (favourites.length > 0) {
+      const recommendedMovieSeed =
+        favourites[generateRandomIndex(favourites.length)];
+      getRecommendedMovies(recommendedMovieSeed.id)
+        .then((data) => {
+          const movies = sanitizeVideoData(data.results);
+          setRecommendedMovies({ recommendedMovieSeed, movies });
+        })
+        .catch((error) => console.log(error));
+    }
+  }, []);
+
   console.log(favourites);
   return (
     <main className="favourites-page">
@@ -18,6 +34,12 @@ const FavouritesPage = () => {
           <h3>No Refunds!</h3>
         </>
       )}
+      {recommendedMovies?.movies?.length > 0 ? (
+        <MoviesContainer
+          title={`If you liked ${recommendedMovies.recommendedMovieSeed.original_title}, you might also like...`}
+          movies={recommendedMovies.movies}
+        />
+      ) : null}
     </main>
   );
 };
