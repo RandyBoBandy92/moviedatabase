adc53a4dfc730935ed70296cd4364e6ac8f0d3b2
Ryan Gulak <ryan.gulak@gmail.com>
2022-01-30 16:54:55 -0800

    removed comments on data structure

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -14,33 +14,6 @@ import {
 } from "../utilities/api";
 import { formatMovieDate, getTrailerKey } from "../utilities/toolbelt.js";
 
-// Data Structure from API:
-// adult: false
-// backdrop_path: "/c6H7Z4u73ir3cIoCteuhJh7UCAR.jpg"
-// belongs_to_collection: null
-// budget: 200000000
-// genres: Array(4) [ {…}, {…}, {…}, … ]
-// homepage: "https://www.marvel.com/movies/the-eternals"
-// id: 524434
-// imdb_id: "tt9032400"
-// original_language: "en"
-// original_title: "Eternals"
-// overview: "The Eternals are a team of ancient aliens who have been living on Earth in secret for thousands of years. When an unexpected tragedy forces them out of the shadows, they are forced to reunite against mankind’s most ancient enemy, the Deviants."
-// popularity: 11902.6
-// poster_path: "/b6qUu00iIIkXX13szFy7d0CyNcg.jpg"
-// production_companies: Array [ {…} ]
-// production_countries: Array [ {…} ]
-// release_date: "2021-11-03"
-// revenue: 401842256
-// runtime: 156
-// spoken_languages: Array(5) [ {…}, {…}, {…}, … ]
-// status: "Released"
-// tagline: "In the beginning..."
-// title: "Eternals"
-// video: false
-// vote_average: 7.3
-// vote_count: 2852
-
 const formatMovieRuntime = (rawMovieRuntime) => {
   // data will arrive as probably a 2 or 3 digit number
   // if it is over 60 minutes, then it should return
