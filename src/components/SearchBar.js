89c72c15bc464ba2ca88549acf0720dc66264ad6
Randy Gulak <randy@randygulak.com>
2022-04-09 11:14:39 -0700

    fixed search bar toggle error, and search now auto

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState, useRef } from "react";
 import { ImSearch } from "react-icons/im";
 import { useNavigate } from "react-router-dom";
 
 function SearchBar() {
   const [query, setQuery] = useState("");
-  const [navOpen, setNavOpen] = useState(false);
+  const [searchTimeoutId, setSearchTimeoutId] = useState("");
+  const [searchOpen, setSearchOpen] = useState(false);
   const ref = useRef();
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (query && navOpen) {
-      const uriQuery = encodeURIComponent(query);
-      navigate(`/search/${uriQuery}`);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      // this is really chunky.. but it works which will have to do for now :(
+      // if i don't account for this edge case, the search bar cannot be closed properly
+      if (
+        e.target.className === "searchButton" ||
+        e.target.nodeName === "svg" ||
+        e.target.nodeName === "path"
+      ) {
+        return;
+      }
+
+      if (ref.current && !ref.current.contains(e.target)) {
+        setSearchOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    // so every time the query changes
+    // i clear any prior searchTimeoutId
+    // then start another one, if we reach half a second without entering new results, we construct uriQuery and navigate
+    clearTimeout(searchTimeoutId);
+    if (query) {
+      const timeoutId = setTimeout(() => {
+        // trim any trailing whitespace on query to prevent error
+        const uriQuery = encodeURIComponent(query.trim());
+        navigate(`/search/${uriQuery}`);
+      }, 500);
+      setSearchTimeoutId(timeoutId);
     }
-    if (!navOpen) {
-      setNavOpen(true);
+  }, [query]);
+
+  const handleClick = (e) => {
+    setSearchOpen(!searchOpen);
+    if (!searchOpen) {
+      // if the nav is opening, focus the search box
       ref?.current?.focus();
     }
   };
-
   return (
     // Both the form, and the button have a handleSubmit function
     // that way, clicking or pressing enter on the keyboard will submit the form
     // this also works on mobile devices
 
-    <form
-      onSubmit={handleSubmit}
-      className={`searchBox ${navOpen ? "active" : ""}`}
-      onBlur={() => setNavOpen(false)}
-    >
+    <div className={`searchBox ${searchOpen ? "active" : ""}`}>
       <input
         className="searchInput"
         ref={ref}
@@ -41,10 +72,10 @@ function SearchBar() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <div onClick={handleSubmit} className="searchButton">
+      <div onClick={handleClick} className="searchButton">
         <ImSearch className="search-icon" />
       </div>
-    </form>
+    </div>
   );
 }
 
